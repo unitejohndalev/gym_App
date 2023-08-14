110fd16c8c286e06228aae2f86b08dffc9cab608
John Dale <unitejohndalev@gmail.com>
2023-08-14 10:28:58 +0800

    finished

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,13 +1,27 @@
+import { SelectedPage } from "@/shared/types";
 import AnchorLink from "react-anchor-link-smooth-scroll"
 
 
 type Props = {
-    page: string;
-}
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Link = ({page,}: Props) => {
+const Link = ({
+  page,
+  selectedPage,
+  setSelectedPage,
+}: Props) => { 
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage
   return (
-<AnchorLink>
+<AnchorLink
+href={`#${lowerCasePage}`}
+onClick={() => setSelectedPage(lowerCasePage)}
+className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+transition duration-500 hover:text-primary-300
+`}
+>
     {page}
 </AnchorLink>
   )
---
+++ src/stores/useSelectedPage.ts
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+
+export const useSelectedPage = create((set) => ({
+    selectedPage: ("home"),
+    setSelected: (prev) => 
+    set((state) => {
+        
+    })
+}))
